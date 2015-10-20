d2b4ff1fda8edd7402be6acddcd7a4e89d57c630
Simon Schwarz <parablack@web.de>
2015-10-20 18:17:05 +0200

    Graphical fix of FullLineDrawer

--- wear/src/main/java/net/parablack/clocktest/watchface/drawer/mode/FullLineDrawer.java
+++ wear/src/main/java/net/parablack/clocktest/watchface/drawer/mode/FullLineDrawer.java
@@ -3,6 +3,7 @@ package net.parablack.clocktest.watchface.drawer.mode;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 
 import net.parablack.clocktest.watchface.drawer.WatchFaceDrawer;
 import net.parablack.clocktest.watchface.drawer.mode.wrapper.SuperTimeWrapper;
@@ -47,9 +48,16 @@ public class FullLineDrawer extends ModeFaceDrawer<SuperTimeWrapper>{
         c.drawRect(0, y, sP, y + height, greenPaint);
         c.drawRect(sP, y, width, y + height, redPaint);
 
-        int a = (int) (currentPercentage * 100);
-        float f = a / 100F;
-        c.drawText(f + "", centerX - yellowPaint.measureText(f + "") / 2, y + 20, yellowPaint);
+        int a = (int) (currentPercentage * 1000);
+        float f = a / 10F;
+
+        Rect bounds = new Rect();
+        yellowPaint.getTextBounds(f + "", 0, 2, bounds);
+        yellowPaint.setTextAlign(Paint.Align.CENTER);
+        int hei = bounds.height();
+        float offset = y + height - ((height - hei) / 2);
+
+        c.drawText(f + "", centerX, offset, yellowPaint);
 
     }
 }
